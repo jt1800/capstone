dfee6b8656d23b70a97a9f8c4d6b7a1f9d42a416
Jacob Thoennes <jt1800@gmail.com>
2017-11-06 21:13:10 -0600

    user import test

--- dataclean.R
+++ dataclean.R
@@ -1,4 +1,22 @@
 
 
-install.packages('jsonlite')
-require(jsonlite)
+#install.packages('jsonlite')
+library(jsonlite)
+library(dplyr)
+
+lines <- readLines(file("D:/code/R/capstone/dataset/user.json"), n = 10000)
+
+users <- data.frame()
+
+for (i in 1:length(lines)){
+  user <- fromJSON(lines[i])
+  user$friends <- length(user$friends)
+  user$elite <- length(user$elite)
+  users <- rbind(users, data.frame(user))
+  if (i%%1000 == 0){
+    print(i)
+  }
+}
+
+
+#user <- stream_in(file("D:/code/R/capstone/dataset/user.json"),pagesize=100000)
